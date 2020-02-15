af359c2961e0c0385e9b6c9155a6696fb91a247f
D3m0n_C0d3r <thepunisheronenonly@gmail.com>
2020-02-15 18:54:55 +0530

    exception added.

--- backend.py
+++ backend.py
@@ -5,32 +5,38 @@ import xml.dom.minidom
 
 class connect():
     def __init__(self):
-        # make connection
-        self.conn = pypyodbc.connect('Driver={SQL Server}; Server=DESKTOP-9A1HCKG;Database=pubs')
+        try:
+            # make connection
+            self.conn = pypyodbc.connect('Driver={SQL Server}; Server=DESKTOP-9A1HCKG;Database=pubs')
 
-        # create cursor objects
-        self.cur = self.conn.cursor()
+            # create cursor objects
+            self.cur = self.conn.cursor()
 
-        # create sql query and store it into variable
-        self.cmd = "SELECT * FROM dbo.sales FOR XML PATH('SALE'), ROOT('SALES')"
+            # create sql query and store it into variable
+            self.cmd = "SELECT * FROM dbo.sales FOR XML PATH('SALE'), ROOT('SALES')"
+        except:
+            print('Error occur while initializing the connection.')
 
 
     def xmlgenerate(self):
-        # execute the sql query and store it into variable
-        self.cur.execute(self.cmd)
-        rows = self.cur.fetchall()
+        try:
+            # execute the sql query and store it into variable
+            self.cur.execute(self.cmd)
+            rows = self.cur.fetchall()
 
 
-        nml = '<?xml version="1.0"?>'
-        i = 0
-        while i < len(rows):
-            nml = nml + str(rows[i][0])
-            i = i + 1 
+            nml = '<?xml version="1.0"?>'
+            i = 0
+            while i < len(rows):
+                nml = nml + str(rows[i][0])
+                i = i + 1 
 
-        self.dom = xml.dom.minidom.parseString(nml)
-        self.dom = self.dom.toprettyxml()
+            self.dom = xml.dom.minidom.parseString(nml)
+            self.dom = self.dom.toprettyxml()
 
-        with open('sales_reports.xml', 'w') as f:
-            f.writelines(self.dom)
+            with open('sales_reports.xml', 'w') as f:
+                f.writelines(self.dom)
 
-        self.conn.close()
+            self.conn.close()
+        except:
+            print('Error occur while generating xml.')
--- sendMail.py
+++ sendMail.py
@@ -3,25 +3,31 @@ from email.message import EmailMessage
 
 class mailsend():
     def __init__(self):
-        self.msg = EmailMessage()
-        self.msg['Subject'] = "Sales report testing"
-        self.msg['From'] = 'siddharth.warwatkar@outlook.com'
-        self.msg['To'] = 'siddevil938@gmail.com'
-        self.msg.set_content("Report attached....")
+        try:
+            self.msg = EmailMessage()
+            self.msg['Subject'] = "Sales report testing"
+            self.msg['From'] = 'siddharth.warwatkar@outlook.com'
+            self.msg['To'] = 'siddevil938@gmail.com'
+            self.msg.set_content("Report attached....")
 
-        files = ['sales_reports.xml']
+            files = ['sales_reports.xml']
 
-        for file in files:
-            with open(file, 'rb') as f:
-                file_data = f.read()
-                file_name = f.name
+            for file in files:
+                with open(file, 'rb') as f:
+                    file_data = f.read()
+                    file_name = f.name
 
-            self.msg.add_attachment(file_data, maintype='application', subtype='octet-stream',filename=file_name)
-    
+                self.msg.add_attachment(file_data, maintype='application', subtype='octet-stream',filename=file_name)
+        except:
+            print('Error occur while initializing the mail.')
+
     def msent(self):
-        with smtplib.SMTP(host='smtp-mail.outlook.com', port=587) as smtp:
-            smtp.ehlo()
-            smtp.starttls()
-            smtp.ehlo()
-            smtp.login('siddharth.warwatkar@outlook.com', 'Devil@931986')
-            smtp.send_message(self.msg)
+        try:
+            with smtplib.SMTP(host='smtp-mail.outlook.com', port=587) as smtp:
+                smtp.ehlo()
+                smtp.starttls()
+                smtp.ehlo()
+                smtp.login('siddharth.warwatkar@outlook.com', 'Devil@931986')
+                smtp.send_message(self.msg)
+        except:
+            print('Error occur while sending mail.')
